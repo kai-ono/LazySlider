ecaa79281027cbacbaaeec556c4b01e11a1b5039
kai-ono <k.oono.a@gmail.com>
2018-01-23 19:15:56 +0900

    wip

--- dest/js/lazy-slider.js
+++ dest/js/lazy-slider.js
@@ -15,6 +15,7 @@ var Element = function () {
     this.itemLen = this.item.length;
     this.itemW = 100 / this.itemLen;
     this.remainingItem = 0;
+    this.dir = true;
     this.dupItemLen = 0;
     this.dupItemLeftLen = 0;
     this.showW = this.itemW * showItem;
