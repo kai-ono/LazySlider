6d21b62e87a33f5efd93a4252fc0315900aea8cb
kai-ono <k.oono.a@gmail.com>
2018-02-26 10:55:25 +0900

    build.

--- dest/js/lazy-slider.js
+++ dest/js/lazy-slider.js
@@ -1,4 +1,4 @@
-(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+(function(){function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s}return e})()({1:[function(require,module,exports){
 'use strict';
 
 var REF = require('./Reference');
@@ -30,7 +30,7 @@ module.exports = {
       if (this.actionLock) return;
       obj.dir = dir;
       var nextCurrent = dir ? ++obj.current : --obj.current;
-      this.Action(nextCurrent, obj);
+      this.Action(nextCurrent, obj, false);
     }
   },
 
@@ -53,9 +53,9 @@ module.exports = {
       naviLi.addEventListener('click', function (e) {
         [].slice.call(e.currentTarget.classList).forEach(function (value) {
           if (value.match(REF.curr) !== null) {
-            var index = Math.ceil(parseInt(value.replace(REF.curr, '')) * _this2.slideNum);
+            var index = Math.ceil(parseInt(value.replace(REF.curr, '')) * (_this2.slideNum - 1));
             obj.dir = true;
-            _this2.Action(index, obj);
+            _this2.Action(index, obj, true);
           };
         });
       });
@@ -89,12 +89,14 @@ module.exports = {
   }
 };
 
-},{"./Reference":3,"./Utils":4}],2:[function(require,module,exports){
+},{"./Reference":3,"./Utils":5}],2:[function(require,module,exports){
 'use strict';
 
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+var SWIPE = require('./Swipe');
 
 var Element = function () {
   function Element(node, showItem) {
@@ -114,6 +116,7 @@ var Element = function () {
     this.naviChildren;
     this.actionCb = [];
     this.dir = true;
+    this.swipe = new SWIPE(this);
     this.Init(showItem);
   }
 
@@ -129,7 +132,7 @@ var Element = function () {
 
 module.exports = Element;
 
-},{}],3:[function(require,module,exports){
+},{"./Swipe":4}],3:[function(require,module,exports){
 'use strict';
 
 module.exports = {
@@ -146,6 +149,273 @@ module.exports = {
 };
 
 },{}],4:[function(require,module,exports){
+'use strict';
+
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+var Swipe = function () {
+  function Swipe(args) {
+    _classCallCheck(this, Swipe);
+
+    this._this = args;
+    console.log(this._this.elm);
+  }
+
+  _createClass(Swipe, [{
+    key: 'Direction',
+    value: function Direction() {
+      var distX = void 0,
+          distY = void 0,
+          r = void 0,
+          swipeAngle = void 0,
+          _ = this;
+
+      distX = _.touchObject.startX - _.touchObject.curX;
+      distY = _.touchObject.startY - _.touchObject.curY;
+      r = Math.atan2(distY, distX);
+
+      swipeAngle = Math.round(r * 180 / Math.PI);
+      if (swipeAngle < 0) {
+        swipeAngle = 360 - Math.abs(swipeAngle);
+      }
+
+      if (swipeAngle <= 45 && swipeAngle >= 0) {
+        return _.options.rtl === false ? 'left' : 'right';
+      }
+      if (swipeAngle <= 360 && swipeAngle >= 315) {
+        return _.options.rtl === false ? 'left' : 'right';
+      }
+      if (swipeAngle >= 135 && swipeAngle <= 225) {
+        return _.options.rtl === false ? 'right' : 'left';
+      }
+      if (_.options.verticalSwiping === true) {
+        if (swipeAngle >= 35 && swipeAngle <= 135) {
+          return 'down';
+        } else {
+          return 'up';
+        }
+      }
+
+      return 'vertical';
+    }
+  }, {
+    key: 'End',
+    value: function End(event) {
+      var _ = this,
+          slideCount,
+          direction;
+
+      _.dragging = false;
+      _.swiping = false;
+
+      if (_.scrolling) {
+        _.scrolling = false;
+        return false;
+      }
+
+      _.interrupted = false;
+      _.shouldClick = _.touchObject.swipeLength > 10 ? false : true;
+
+      if (_.touchObject.curX === undefined) {
+        return false;
+      }
+
+      if (_.touchObject.edgeHit === true) {
+        _.$slider.trigger('edge', [_, _.Direction()]);
+      }
+
+      if (_.touchObject.swipeLength >= _.touchObject.minSwipe) {
+
+        direction = _.Direction();
+
+        switch (direction) {
+
+          case 'left':
+          case 'down':
+
+            slideCount = _.options.swipeToSlide ? _.checkNavigable(_.currentSlide + _.getSlideCount()) : _.currentSlide + _.getSlideCount();
+
+            _.currentDirection = 0;
+
+            break;
+
+          case 'right':
+          case 'up':
+
+            slideCount = _.options.swipeToSlide ? _.checkNavigable(_.currentSlide - _.getSlideCount()) : _.currentSlide - _.getSlideCount();
+
+            _.currentDirection = 1;
+
+            break;
+
+          default:
+
+        }
+
+        if (direction != 'vertical') {
+
+          _.slideHandler(slideCount);
+          _.touchObject = {};
+          _.$slider.trigger('swipe', [_, direction]);
+        }
+      } else {
+
+        if (_.touchObject.startX !== _.touchObject.curX) {
+
+          _.slideHandler(_.currentSlide);
+          _.touchObject = {};
+        }
+      }
+    }
+  }, {
+    key: 'Handler',
+    value: function Handler(event, obj) {
+      console.log({
+        this: this,
+        evt: obj.action,
+        obj: obj
+      });
+
+      var _ = this;
+
+      if (_.options.swipe === false || 'ontouchend' in document && _.options.swipe === false) {
+        return;
+      } else if (_.options.draggable === false && event.type.indexOf('mouse') !== -1) {
+        return;
+      }
+
+      _.touchObject.fingerCount = event.originalEvent && event.originalEvent.touches !== undefined ? event.originalEvent.touches.length : 1;
+
+      _.touchObject.minSwipe = _.listWidth / _.options.touchThreshold;
+
+      if (_.options.verticalSwiping === true) {
+        _.touchObject.minSwipe = _.listHeight / _.options.touchThreshold;
+      }
+
+      switch (event.data.action) {
+
+        case 'start':
+          break;
+
+        case 'move':
+          break;
+
+        case 'end':
+          break;
+
+      }
+    }
+  }, {
+    key: 'Move',
+    value: function Move(event) {
+      var _ = this,
+          edgeWasHit = false,
+          curLeft,
+          Direction,
+          swipeLength,
+          positionOffset,
+          touches,
+          verticalSwipeLength;
+
+      touches = event.originalEvent !== undefined ? event.originalEvent.touches : null;
+
+      if (!_.dragging || _.scrolling || touches && touches.length !== 1) {
+        return false;
+      }
+
+      curLeft = _.getLeft(_.currentSlide);
+
+      _.touchObject.curX = touches !== undefined ? touches[0].pageX : event.clientX;
+      _.touchObject.curY = touches !== undefined ? touches[0].pageY : event.clientY;
+
+      _.touchObject.swipeLength = Math.round(Math.sqrt(Math.pow(_.touchObject.curX - _.touchObject.startX, 2)));
+
+      verticalSwipeLength = Math.round(Math.sqrt(Math.pow(_.touchObject.curY - _.touchObject.startY, 2)));
+
+      if (!_.options.verticalSwiping && !_.swiping && verticalSwipeLength > 4) {
+        _.scrolling = true;
+        return false;
+      }
+
+      if (_.options.verticalSwiping === true) {
+        _.touchObject.swipeLength = verticalSwipeLength;
+      }
+
+      Direction = _.Direction();
+
+      if (event.originalEvent !== undefined && _.touchObject.swipeLength > 4) {
+        _.swiping = true;
+        event.preventDefault();
+      }
+
+      positionOffset = (_.options.rtl === false ? 1 : -1) * (_.touchObject.curX > _.touchObject.startX ? 1 : -1);
+      if (_.options.verticalSwiping === true) {
+        positionOffset = _.touchObject.curY > _.touchObject.startY ? 1 : -1;
+      }
+
+      swipeLength = _.touchObject.swipeLength;
+
+      _.touchObject.edgeHit = false;
+
+      if (_.options.infinite === false) {
+        if (_.currentSlide === 0 && Direction === 'right' || _.currentSlide >= _.getDotCount() && Direction === 'left') {
+          swipeLength = _.touchObject.swipeLength * _.options.edgeFriction;
+          _.touchObject.edgeHit = true;
+        }
+      }
+
+      if (_.options.vertical === false) {
+        _.swipeLeft = curLeft + swipeLength * positionOffset;
+      } else {
+        _.swipeLeft = curLeft + swipeLength * (_.$list.height() / _.listWidth) * positionOffset;
+      }
+      if (_.options.verticalSwiping === true) {
+        _.swipeLeft = curLeft + swipeLength * positionOffset;
+      }
+
+      if (_.options.fade === true || _.options.touchMove === false) {
+        return false;
+      }
+
+      if (_.animating === true) {
+        _.swipeLeft = null;
+        return false;
+      }
+
+      _.setCSS(_.swipeLeft);
+    }
+  }, {
+    key: 'Start',
+    value: function Start(event) {
+      var _ = this,
+          touches;
+
+      _.interrupted = true;
+
+      if (_.touchObject.fingerCount !== 1 || _.slideCount <= _.options.slidesToShow) {
+        _.touchObject = {};
+        return false;
+      }
+
+      if (event.originalEvent !== undefined && event.originalEvent.touches !== undefined) {
+        touches = event.originalEvent.touches[0];
+      }
+
+      _.touchObject.startX = _.touchObject.curX = touches !== undefined ? touches.pageX : event.clientX;
+      _.touchObject.startY = _.touchObject.curY = touches !== undefined ? touches.pageY : event.clientY;
+
+      _.dragging = true;
+    }
+  }]);
+
+  return Swipe;
+}();
+
+module.exports = Swipe;
+
+},{}],5:[function(require,module,exports){
 'use strict';
 
 module.exports = {
@@ -168,7 +438,7 @@ module.exports = {
   }
 };
 
-},{}],5:[function(require,module,exports){
+},{}],6:[function(require,module,exports){
 'use strict';
 
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
@@ -298,14 +568,16 @@ var LazySlider = function () {
     }
   }, {
     key: 'Action',
-    value: function Action(index, obj) {
+    value: function Action(index, obj, isNaviEvent) {
       var _this2 = this;
 
       clearTimeout(obj.autoID);
       this.actionLock = true;
 
-      for (var i = 1; i < this.slideNum; i++) {
-        index = obj.dir ? ++index : --index;
+      if (typeof isNaviEvent === 'undefined' || !isNaviEvent) {
+        for (var i = 1; i < this.slideNum; i++) {
+          index = obj.dir ? ++index : --index;
+        }
       }
 
       var isLast = function isLast(item) {
@@ -337,7 +609,7 @@ var LazySlider = function () {
       var timer = function timer() {
         obj.autoID = setTimeout(function () {
           obj.dir = true;
-          _this3.Action(++obj.current, obj);
+          _this3.Action(++obj.current, obj, false);
         }, _this3.interval);
       };
 
@@ -357,4 +629,4 @@ var LazySlider = function () {
 
 window.LazySlider = LazySlider;
 
-},{"./mod/Creates":1,"./mod/Element":2,"./mod/Reference":3,"./mod/Utils":4}]},{},[5]);
+},{"./mod/Creates":1,"./mod/Element":2,"./mod/Reference":3,"./mod/Utils":5}]},{},[6]);
