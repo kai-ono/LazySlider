2d380daf19f6370ab1d473d3186b180f6084610f
kai-ono <k.oono.a@gmail.com>
2018-04-18 18:34:19 +0900

    fix test error.

--- gulpfile.js
+++ gulpfile.js
@@ -7,7 +7,7 @@ var fs = require('fs');
 
 // gulpディレクトリ内のタスクファイル(.js)を全て取得します
 fs.readdirSync(path).forEach(function(file) {
-  if(/.*\.js$/.test(file)) {
-    require(path + '/' + file)(gulp);
-  }
+    if (/.*\.js$/.test(file)) {
+        require(path + '/' + file)(gulp);
+    }
 });
